94479c075be1ccdb144efce34f51dc4c56df7819
Manohar <manoharsanthosh1.com>
2026-03-29 23:05:28 +0100

    login popup update

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,16 @@
 console.log("RideShare loaded");
+function openLogin() {
+  document.getElementById("loginModal").style.display = "block";
+}
+
+function closeLogin() {
+  document.getElementById("loginModal").style.display = "none";
+}
+
+// Close when clicking outside
+window.onclick = function(event) {
+  let modal = document.getElementById("loginModal");
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
